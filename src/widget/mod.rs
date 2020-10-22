c573dc37ab7f3750c7e70378d30fae9e78c0e297
DoctorVWA <doctorvwa@PCDOCTORVWA.localdomain>
2020-10-22 19:31:50 -0300

    project base structure

---
+++ src/widget/mod.rs
@@ -0,0 +1,9 @@
+mod button;
+mod text_box;
+mod inventory;
+mod progress_bar;
+
+pub use button::Button;
+pub use text_box::TextBox;
+pub use inventory::Inventory;
+pub use progress_bar::ProgressBar;
